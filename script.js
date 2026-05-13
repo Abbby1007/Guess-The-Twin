d7c3b2731e20e793e2cd0c6638bc00d14be934a6
Abbby1007 <Abigailloken25@gmail.com>
2026-05-13 21:52:01 +0900

    More updates to the question System

--- script.js
+++ script.js
@@ -22,16 +22,30 @@ const questions = () =>{
     let test = 0;
     const photos = ["d1.jpg","d2.jpg","d3.jpg"];
     let index = Math.floor(Math.random() * 3);
-    document.getElementById("image").innerHTML = `Number: ${index} <br> <img src="${photos[index]}">`;
-        document.getElementById("Test2").innerHTML = `Array length: ${problems.length - 1}. Array:${problems}`;
+    document.getElementById("Test").innerHTML=`${index}`;
         for(let i = 0; i< problems.length; i++){
         if(problems[i] === index){
             test = 1;
         }
     }
             if(test != 1){
-            problems.push(num);
+            problems.push(index);
+            if(index === 0){
+                question1();
+            }
+            else if(index === 1){
+                question2();
+            }
+            else if(index === 2){
+                question3();
+            }
+
+            else{
+                document.getElementById("question").innerHTML=`NUMBER ISN"T HERE :(`;   
+            }
         }
+
+
 // Determine if the user is correct
     let points = 0;
 if(document.querySelector("rAbigail").checked){
@@ -53,7 +67,6 @@ document.getElementById("Test2").innerHTML = `IS CORRECT`;
 else{
 document.getElementById("Test2").innerHTML = `IS Wrong`;
 }
-        document.getElementById("Test2").innerHTML = `Array length: ${problems.length - 1}. Array:${problems}`;
 }
 
 const checkQuestion = (num) =>{
@@ -76,21 +89,78 @@ if(num === 1){
     if(points === 5){
         document.getElementById("Test2").innerHTML = `IS CORRECT`;
     }
-    else if(points === 1){
-        document.getElementById("Test2").innerHTML = `ONE`;
-    }
     else{
         document.getElementById("Test2").innerHTML = `Is Wrong`;
     }
 }
-
-
+    else{
+        document.getElementById("Test2").innerHTML = `Not a valid Numbers`;
+    }
 }
-// document.getElementById("Test2").innerHTML = `Array length: ${problems.length - 1}. Array:${problems}`;
 
-
+//Question 1 Function
 const question1 = () => {
     document.getElementById("question").innerHTML=`<img src="d1.jpg"> <br> 
+    <p> Who is on the Right</p>
+    <form>
+        
+        <!-- Right Abigail equals 1 point -->
+        <input type="checkbox" id="rAbigail" name="rAbigail" value="rAbigail">
+        <label for="rAbigail">Abigail</label> 
+
+        <!-- CORRECT -->
+         <!-- Right Adriyel equals 2 points -->
+        <input type="checkbox" id="rAdriyel" name="rAdriyel" value="rAdriyel">
+        <label for="rAdriyel">Adriyel</label>
+    </form>
+<!-- 5 is the correct total -->
+    <p> Who is on the Left</p>
+    <form>
+        <!-- CORRECT -->
+        <!-- Left Abigail equals 3 points -->
+        <input type="checkbox" id="lAbigail" name="lAbigail" value="lAbigail">
+        <label for="lAbigail">Abigail</label> 
+        <!-- left Adriyel equals 4 points -->
+        <input type="checkbox" id="lAdriyel" name="lAdriyel" value="lAdriyel">
+        <label for="lAdriyel">Adriyel</label>
+    </form>
+<br>
+    <button onclick="checkQuestion(1)">Submit</button>`;
+}
+
+//Question 2 Function
+const question2 = () => {
+    document.getElementById("question").innerHTML=`<img src="d2.jpg"> <br> 
+    <p> Who is on the Right</p>
+    <form>
+        
+        <!-- Right Abigail equals 1 point -->
+        <input type="checkbox" id="rAbigail" name="rAbigail" value="rAbigail">
+        <label for="rAbigail">Abigail</label> 
+
+        <!-- CORRECT -->
+         <!-- Right Adriyel equals 2 points -->
+        <input type="checkbox" id="rAdriyel" name="rAdriyel" value="rAdriyel">
+        <label for="rAdriyel">Adriyel</label>
+    </form>
+<!-- 5 is the correct total -->
+    <p> Who is on the Left</p>
+    <form>
+        <!-- CORRECT -->
+        <!-- Left Abigail equals 3 points -->
+        <input type="checkbox" id="lAbigail" name="lAbigail" value="lAbigail">
+        <label for="lAbigail">Abigail</label> 
+        <!-- left Adriyel equals 4 points -->
+        <input type="checkbox" id="lAdriyel" name="lAdriyel" value="lAdriyel">
+        <label for="lAdriyel">Adriyel</label>
+    </form>
+<br>
+    <button onclick="checkQuestion(1)">Submit</button>`;
+}
+
+//Question 2 Function
+const question3 = () => {
+    document.getElementById("question").innerHTML=`<img src="d3.jpg"> <br> 
     <p> Who is on the Right</p>
     <form>
         
